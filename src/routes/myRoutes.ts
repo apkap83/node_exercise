9bfbea12a011d999f6d521d5790316d1508e80f5
apkap83 <ap.kapetanios@gmail.com>
2024-01-27 21:10:23 +0200

    Creation of requests 3 and 4

--- src/routes/myRoutes.ts
+++ src/routes/myRoutes.ts
@@ -1,27 +1,66 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { Op } from "sequelize";
 
 import { User, Message } from "../services/dbService/initDBAndModels";
-
-import { addUserInDB, AddUserInDBArgs } from "../services/userService/user";
-import {
-  addMessageInDB,
-  AddMessageInDBArgs,
-} from "../services/messageService/message";
-
 import { loadUsersFromExcel, loadMessagesFromExcel } from "../utils/helpers";
 
 export const myRouter = express.Router();
 
-interface UserSheetData {
-  firstName: string;
-  surname: string;
-  dateOfBirth: Date;
-  gender: string;
-  userName: string;
-}
+myRouter.post("/feedDB", async (req: Request, res: Response) => {
+  try {
+    await loadUsersFromExcel();
+    await loadMessagesFromExcel();
+    return res.status(200).send("OK!");
+  } catch (error) {
+    return res.status(500).send("Error during load of excel to DB");
+  }
+});
 
-myRouter.post("/feedDB", async (req, res) => {
-  await loadUsersFromExcel();
-  await loadMessagesFromExcel();
-  return res.status(200).send("OK!");
+myRouter.get("/users", async (req: Request, res: Response) => {
+  const { firstName, surname, gender } = req.query;
+
+  const whereClause: any = {};
+
+  if (firstName) {
+    whereClause.firstName = firstName;
+  }
+
+  if (surname) {
+    whereClause.surname = surname;
+  }
+
+  if (gender) {
+    whereClause.gender = gender;
+  }
+
+  try {
+    const users = await User.findAll({
+      where: whereClause,
+    });
+
+    res.json(users);
+  } catch (error) {
+    return res.status(500).send("Error during retrieval of users");
+  }
+});
+
+myRouter.get("/messages", async (req: Request, res: Response) => {
+  try {
+    const { user1Id, user2Id } = req.query;
+
+    const messages = await Message.findAll({
+      where: {
+        [Op.or]: [
+          { sender: user1Id, receiver: user2Id },
+          { sender: user2Id, receiver: user1Id },
+        ],
+      },
+      order: [["timestampSent", "DESC"]],
+    });
+
+    res.json(messages);
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
